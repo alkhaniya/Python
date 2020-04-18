93e0a10d0d9e4706218737c3107069336bff9318
alkhaniya <aditya.alkhaniya@gmail.com>
2020-04-18 08:16:12 +0530

    for

--- forLoop.py
+++ forLoop.py
@@ -1,4 +1,6 @@
-i=1
 n=input("Enter the number to which you want to print natural numbers: ")
-for i in range(n):
-    print(i+1) 
+for i in range(1,11,2):
+    print(n*i)
+
+for j in ['Aditya','Shrey','Abhi']:
+    print(j)
