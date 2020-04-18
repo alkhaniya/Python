2dba2093007b7eee5ae2e64c3ec3b1b2239e0ccc
alkhaniya <aditya.alkhaniya@gmail.com>
2020-04-18 10:18:52 +0530

    whileNested

--- whileLoop.py
+++ whileLoop.py
@@ -1,12 +1,27 @@
-i=1
-j=1
-n=input("Enter the number: ")
-while i<=10:
-    print(n*i)
-    i=i+1
+# i=0
+# j=1
 
-while j<=10:
-    print(n**j)
-    j+=1
+# n=input("Enter the number: ")
+# while i<=100:
+#     print(n*i)
+#     i=i+1
+
+# while j<=10:
+#     print(n**j)
+#     j+=1
+
+i=0
+j=0
+
+while i<10:
+    j=0
+    while j<10:
+        print(str(i) + str(j))
+        #print(j)
+        j+=1
+    i+=1
+
+
+
 
 
