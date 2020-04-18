6dc44124eac674ec4e57e91d377cc217ae3f5bb6
alkhaniya <aditya.alkhaniya@gmail.com>
2020-04-18 19:44:43 +0530

    lists 2

--- list_2.py
+++ list_2.py
@@ -1,4 +1,5 @@
 arr=[1,4,55,33,88,66,50]
+arr2=[]
 
 for i in arr:
     print(i*2)
@@ -7,13 +8,16 @@ for i in arr:
 print("reverse")
 for i in range(len(arr),0,-1):
     print(arr[i-1]*2)
+    arr2.append(arr[i-1])
 
 print("Updation")
-
 for i in range(len(arr)):
     arr[i]*=2
 
-for j in range(len(arr),0,-1):
-    print(arr[j-1]),
-    
+
+
+print(arr2)
+# print("reverse updation")
+# for i in range(len(arr)):
+#     arr[i]=arr[]
 
